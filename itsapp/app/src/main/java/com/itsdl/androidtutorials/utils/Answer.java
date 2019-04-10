810726b135d5e24ec94abd02ed529cb960c17fd2
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-04-10 21:01:10 +0700

    Add request problem
    add request

---
+++ itsapp/app/src/main/java/com/itsdl/androidtutorials/utils/Answer.java
@@ -0,0 +1,18 @@
+package com.itsdl.androidtutorials.utils;
+
+public class Answer {
+    private int answer_id;
+    private String answer_content;
+    private boolean result;
+
+
+    public Answer(int answer_id, String answer_content, boolean result) {
+        this.answer_id = answer_id;
+        this.answer_content = answer_content;
+        this.result = result;
+    }
+
+    public String getContent() {
+        return answer_content;
+    }
+}
