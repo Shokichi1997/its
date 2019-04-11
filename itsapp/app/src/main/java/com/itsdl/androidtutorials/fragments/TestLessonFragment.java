200658398a96b9367ac75069ad54bf6c1ad3c549
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-04-11 23:54:09 +0700

    Update problem fragment

--- itsapp/app/src/main/java/com/itsdl/androidtutorials/fragments/TestLessonFragment.java
+++ itsapp/app/src/main/java/com/itsdl/androidtutorials/fragments/TestLessonFragment.java
@@ -1,18 +1,23 @@
 package com.itsdl.androidtutorials.fragments;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatRadioButton;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
@@ -38,10 +43,14 @@ public class TestLessonFragment extends Fragment implements View.OnClickListener
     private Toolbar toolbar;
     private ProgressBar progressBarProblem;
     private Context context;
+    private Question questionAll = null;
+    private ArrayList<Integer> checkArr = null;
+    private EditText edtAnswerUser;
+    private View root;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_test_lesson,container,false);
+        root = inflater.inflate(R.layout.fragment_test_lesson,container,false);
         addControls(root);
         addEvents(root);
         return root;
@@ -51,6 +60,7 @@ public class TestLessonFragment extends Fragment implements View.OnClickListener
     private void addControls(View root) {
         //add tool bar
         context = getContext();
+        checkArr = new ArrayList<>();
         toolbar = root.findViewById(R.id.test_toolbar);
         ((AppCompatActivity )getActivity()).setSupportActionBar(toolbar);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Problem - Quiz");
@@ -132,7 +142,7 @@ public class TestLessonFragment extends Fragment implements View.OnClickListener
     }
 
     private void setContentProblem(Question question) {
-        llAnswer.removeAllViews();
+        initQuestionInfo(question);
         lblQuestionContent.setText(question.getContent());
         int type = question.getType_qs();
         ArrayList<Answer> answers = question.getAnswers();
@@ -140,6 +150,15 @@ public class TestLessonFragment extends Fragment implements View.OnClickListener
         int index = 100;
         if(type == 1){
             group = new RadioGroup(context);
+            group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    if(!checkArr.isEmpty()){
+                        checkArr.clear();
+                    }
+                    checkArr.add(checkedId-100);
+                }
+            });
             llAnswer.addView(group);
         }
         int size = answers.size();
@@ -149,27 +168,122 @@ public class TestLessonFragment extends Fragment implements View.OnClickListener
                     RadioButton radioButton = new RadioButton(context);
                     radioButton.setText(answers.get(i).getContent());
                     radioButton.setId(index+i);
+                    radioButton.setTag("radio"+index+i);
                     group.addView(radioButton);
                     break;
                 case 2:
-                    CheckBox box = new CheckBox(context);
+                    final CheckBox box = new CheckBox(context);
                     box.setText(answers.get(i).getContent());
                     box.setId(index+i);
+                    box.setTag("checkbox"+index+i);
+                    box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            if(isChecked){
+                                checkArr.add(box.getId());
+                            }
+                            else {
+                                int size = checkArr.size();
+                                for(int i=0;i<size;i++){
+                                    if(box.getId() == checkArr.get(i)){
+                                        checkArr.remove(i);
+                                        size--;
+                                    }
+                                }
+                            }
+                        }
+                    });
+
                     llAnswer.addView(box);
                     break;
                 case 3:
-                    EditText editText = new EditText(context);
-                    editText.setInputType(1);
-                    editText.setId(index+i);
-                    llAnswer.addView(editText);
+                    edtAnswerUser = new EditText(context);
+                    edtAnswerUser.setInputType(1);
+                    edtAnswerUser.setId(index+i);
+                    llAnswer.addView(edtAnswerUser);
                     break;
             }
         }
 
     }
 
-    private void checkAnswer() {
+    private void initQuestionInfo(Question question) {
+        questionAll = question;
+        llAnswer.removeAllViews();
+        if(!checkArr.isEmpty()){
+            checkArr.clear();
+        }
+        if(question.getType_qs()==3){
+            if(edtAnswerUser!=null){
+                edtAnswerUser.setText("");
+            }
 
+    }
+    }
+
+    private void checkAnswer() {
+        if(questionAll!=null){
+            int type_qs = questionAll.getType_qs();
+            switch (type_qs){
+                case 1:
+                    checkAnswerForRadioButton();
+                    break;
+                case 2:
+                    checkAnswerForCheckBox();
+                    break;
+                case 3:
+                    if(edtAnswerUser!=null){
+                        String answerUser = edtAnswerUser.getText().toString();
+                        if(!answerUser.isEmpty()){
+                            Toast.makeText(getContext(),answerUser,Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void checkAnswerForCheckBox() {
+        if(!checkArr.isEmpty()){
+            ArrayList<Answer> answers = questionAll.getAnswers();
+            ArrayList<Integer> trueAnswerBox = new ArrayList<>();
+            for (int i = 0;i<answers.size();i++){
+                if(answers.get(i).getResult()==1){
+                    trueAnswerBox.add(i);
+                }
+            }
+            int numberTrue = 0;
+            for(int i = 0;i<trueAnswerBox.size();i++){
+                for(int id:checkArr){
+                    if(trueAnswerBox.get(i).equals(id)){
+                        numberTrue++;
+                    }
+                }
+            }
+            if(numberTrue==answers.size()){
+                //todo
+            }
+
+        }
+    }
+
+    private void checkAnswerForRadioButton() {
+        if(!checkArr.isEmpty()){
+            ArrayList<Answer> answers = questionAll.getAnswers();
+            int trueAnswer = -1;
+            for (int i = 0;i<answers.size();i++){
+                if(answers.get(i).getResult()==1){
+                    trueAnswer = i;
+                    break;
+                }
+            }
+            if(checkArr.get(0)== trueAnswer){
+                root.findViewWithTag("radio"+100+trueAnswer).setBackgroundColor(Color.GREEN);
+            }
+            else {
+                root.findViewWithTag("radio"+100+checkArr.get(0)).setBackgroundColor(Color.RED);
+            }
+        }
     }
 
     private void showHint() {
@@ -179,4 +293,6 @@ public class TestLessonFragment extends Fragment implements View.OnClickListener
     private void showSolution() {
 
     }
+
+
 }
--- itsapp/app/src/main/java/com/itsdl/androidtutorials/utils/Answer.java
+++ itsapp/app/src/main/java/com/itsdl/androidtutorials/utils/Answer.java
@@ -3,10 +3,10 @@ package com.itsdl.androidtutorials.utils;
 public class Answer {
     private int answer_id;
     private String answer_content;
-    private boolean result;
+    private int result;
 
 
-    public Answer(int answer_id, String answer_content, boolean result) {
+    public Answer(int answer_id, String answer_content, int result) {
         this.answer_id = answer_id;
         this.answer_content = answer_content;
         this.result = result;
@@ -14,5 +14,17 @@ public class Answer {
 
     public String getContent() {
         return answer_content;
+    }
+
+    public int getAnswer_id() {
+        return answer_id;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
     }
 }
